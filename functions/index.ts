1f0efcea3f22b5fd77205fc36662692fef9dc9d3
aljazdolenc <aljazdolenc5@gmail.com>
2024-04-03 23:24:29 +0200

    changed function syntax

--- functions/index.ts
+++ functions/index.ts
@@ -3,20 +3,18 @@ interface Env {
     TOKEN: string;
 }
 
-export default {
-    async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-        const key = new Request( new URL(request.url).toString(), request);
-        let response = await caches.default.match(key);
+export const onRequest: PagesFunction<Env> = async ({request, env}) => {
+    const key = new Request(new URL(request.url).toString(), request);
+    let response = await caches.default.match(key);
 
-        if (response) {
-            return response;
-        }
-
-        return fetch(env.ENDPOINT, {
-            headers: {
-                Authorization: `Bearer ${ env.TOKEN }`,
-            },
-        }).then(({status}) => new Response(JSON.stringify(response), {status}))
-            .catch(({message}) => new Response(JSON.stringify({message}), {status: 500}));
+    if (response) {
+        return response;
     }
+
+    return fetch(env.ENDPOINT, {
+        headers: {
+            Authorization: `Basic ${ env.TOKEN }`,
+        },
+    }).then(({status}) => new Response(JSON.stringify(response), {status}))
+        .catch(({message}) => new Response(JSON.stringify({message}), {status: 500}));
 }
