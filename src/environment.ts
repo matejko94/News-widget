3ad9ecee2117213c462eb90474fb6484bd7089a9
matejko94 <matej.senozetnik@gmail.com>
2024-05-31 19:31:42 +0200

    first version

--- src/environment.ts
+++ src/environment.ts
@@ -8,6 +8,18 @@ export const environment = {
             url:'https://midas.ijs.si/elasticsearch-sgd/science/_search',
             auth: btoa('elastic_searchpoint:9GWd1yPhSRxvP7JTrZ')
         },
+        science_count: {
+            url:'https://midas.ijs.si/elasticsearch-sgd/science/_count',
+            auth: btoa('elastic_searchpoint:9GWd1yPhSRxvP7JTrZ')
+        },
+        media_count: {
+            url:'https://midas.ijs.si/elasticsearch-sgd/media/_count',
+            auth: btoa('elastic_searchpoint:9GWd1yPhSRxvP7JTrZ')
+        },
+        policy_count: {
+            url:'https://midas.ijs.si/elasticsearch-sgd/policy/_count',
+            auth: btoa('elastic_searchpoint:9GWd1yPhSRxvP7JTrZ')
+        },
         googleMaps: {
             // TODO: change this to your own API key
             apiKey: 'AIzaSyB1zNBJl3y5xfGP5wJ8ZSfrbvj8cX6FkEc'
--- src/pages/live-reporting/live-reporting.page.ts
+++ src/pages/live-reporting/live-reporting.page.ts
@@ -4,11 +4,14 @@ import {Chart, registerables} from 'chart.js';
 import {ChoroplethController, ColorScale, GeoFeature, ProjectionScale} from 'chartjs-chart-geo';
 import * as topojson from 'topojson-client';
 import {Feature, FeatureCollection, Geometry} from 'geojson';
-import {catchError, map, Observable, of, shareReplay} from "rxjs";
+//import {BehaviorSubject, catchError, map, Observable, of, shareReplay} from "rxjs";
+import { BehaviorSubject, delay, EMPTY, filter, map, Observable, pairwise, shareReplay, startWith, switchMap, tap } from "rxjs";
 import {environment} from "../../environment";
 import {ScienceDto} from "../../domain/news/entity/science-dto.interface";
 import {ScienceItem} from "../../domain/news/entity/science-item.interface";
 import {countryCodes} from "../../resorces/country_mapper"
+import { DataService } from '../../utils/data.service';
+import {AsyncPipe, SlicePipe} from "@angular/common";
 
 // Register the necessary Chart.js components and the Geo chart controller
 Chart.register(...registerables, ChoroplethController, ColorScale, GeoFeature, ProjectionScale);
@@ -16,64 +19,78 @@ Chart.register(...registerables, ChoroplethController, ColorScale, GeoFeature, P
 @Component({
     standalone: true,
     selector: 'app-live-reporting',
-    templateUrl: './app.component.html',
+    imports: [AsyncPipe, SlicePipe],
+    template: `
+        <div style="width:100%;">
+            <canvas id="geoChart" #geoChart></canvas>
+            <div class="grid grid-cols-4 h-container grid-container">
+                <div class="centered-element">
+                    <b>{{ textToDisplay$ | async }}</b>
+                    <p> Publications in the period</p>
+                </div>
+                <div class="centered-element">
+                    <b>{{ textPercentageToDisplay$ | async }}</b>
+                    <p>Compound annual growth rate</p>
+                </div>
+                <div class="centered-element">
+                    <b>{{ textMediaToDisplay$ | async }}</b>
+                    <p>International collaboration</p>
+                </div>
+                <div class="centered-element">
+                    <b>{{ textPolicyToDisplay$ | async }}</b>
+                    <p>Academic-corporate collaboration</p>
+                </div>
+
+            </div>
+
+        </div>`,
     styleUrls: ['./app.component.css']
 })
 
 export default class LiveReportingPage implements AfterViewInit {
 
     public sdg = input.required<string>();
-    constructor(private http: HttpClient) {
+    constructor(private http: HttpClient, private dataService: DataService) {
     }
 
     canvas: any;
     ctx: any;
     @ViewChild("geoChart") geoChart!: ElementRef;
+    public textToDisplay$  =  new BehaviorSubject('0');
+    public textMediaToDisplay$  =  new BehaviorSubject('0');
+    public textPolicyToDisplay$  =  new BehaviorSubject('0');
+    public textPercentageToDisplay$  =  new BehaviorSubject('0 %');
 
 
     ngAfterViewInit() {
         this.createGeoChart();
+
+        try {
+            this.dataService.getScienceCount(this.sdg()).subscribe(response => {
+                console.log(response)
+                this.textToDisplay$.next(response.toLocaleString('en-US'))
+
+                this.dataService.getScienceAllCount(this.sdg()).subscribe(res => {
+                    console.log(res)
+                    const p = Math.round((response/res)*100).toFixed(2)
+                    this.textPercentageToDisplay$.next(`${p} %`)
+                })
+                //this.textToDisplay$.next(false)
+            })
+
+            this.dataService.getMediaCount(this.sdg()).subscribe(response => {
+                this.textMediaToDisplay$.next(response.toLocaleString('en-US'))
+            })
+
+            this.dataService.getPolicyCount(this.sdg()).subscribe(response => {
+                console.log(response)
+                this.textPolicyToDisplay$.next(response.toLocaleString('en-US'))
+            })
+        } catch (error) {
+            console.error('Error creating Geo chart:', error);
+        }
     }
 
-    public getNews(sdg: string): Observable<ScienceItem[]> {
-        return this.http.post<ScienceDto>(
-            environment.api.science.url,
-            {
-                "size": 0,
-                "query": {
-                    "bool": {
-                        "filter": [
-                            {
-                                "match": {
-                                    "SDG.keyword": `SDG ${sdg}`
-                                }
-                            }
-                        ]
-                    }
-                },
-                "aggs": {
-                    "countries": {
-                        "terms": {
-                            "field": "authorships.institutions.country_code.keyword",
-                            "size": 200
-                        }
-                    }
-                }
-            },
-            {
-                headers: new HttpHeaders({
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Basic ' + environment.api.news.auth,
-                }),
-            }).pipe(
-            map(response => response.aggregations.countries.buckets),
-            catchError(e => {
-                console.error('failed to fetch data', e);
-                return of([])
-            }),
-            shareReplay(1)
-        )
-    }
 
     public async showMaps(scienceItem: ScienceItem[]) {
 
@@ -163,10 +180,7 @@ export default class LiveReportingPage implements AfterViewInit {
 
     async createGeoChart() {
         try {
-
-            this.getNews(this.sdg()).subscribe(science => this.showMaps(science))
-
-
+            this.dataService.getScience(this.sdg()).subscribe(science => this.showMaps(science))
         } catch (error) {
             console.error('Error creating Geo chart:', error);
         }
