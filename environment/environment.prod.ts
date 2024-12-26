4de359059c01ab21765ec9be1b8c9fd46c149304
matej-senozetnik <matej.senozetnik.extern@sixt.com>
2024-12-26 18:26:16 +0100

    update

--- environment/environment.prod.ts
+++ environment/environment.prod.ts
@@ -3,27 +3,27 @@ export const environment = {
     api: {
         news: {
             url:'https://midas.ijs.si/elasticsearch-sgd/media/_search',
-            auth: btoa('elastic_searchpoint:dOYHbCMqM16+JKUPqFM4')
+            auth: btoa('elastic_searchpoint:9GWd1yPhSRxvP7JTrZ')
         },
         science: {
             url:'https://midas.ijs.si/elasticsearch-sgd/science/_search',
-            auth: btoa('elastic_searchpoint:dOYHbCMqM16+JKUPqFM4')
+            auth: btoa('elastic_searchpoint:9GWd1yPhSRxvP7JTrZ')
         },
         innovation: {
             url:'https://midas.ijs.si/elasticsearch-sgd/innovations_v2/_search',
-            auth: btoa('elastic_searchpoint:dOYHbCMqM16+JKUPqFM4')
+            auth: btoa('elastic_searchpoint:9GWd1yPhSRxvP7JTrZ')
         },
         science_count: {
             url:'https://midas.ijs.si/elasticsearch-sgd/science/_count',
-            auth: btoa('elastic_searchpoint:dOYHbCMqM16+JKUPqFM4')
+            auth: btoa('elastic_searchpoint:9GWd1yPhSRxvP7JTrZ')
         },
         media_count: {
             url:'https://midas.ijs.si/elasticsearch-sgd/media/_count',
-            auth: btoa('elastic_searchpoint:dOYHbCMqM16+JKUPqFM4')
+            auth: btoa('elastic_searchpoint:9GWd1yPhSRxvP7JTrZ')
         },
         policy_count: {
             url:'https://midas.ijs.si/elasticsearch-sgd/policy/_count',
-            auth: btoa('elastic_searchpoint:dOYHbCMqM16+JKUPqFM4')
+            auth: btoa('elastic_searchpoint:9GWd1yPhSRxvP7JTrZ')
         },
         googleMaps: {
             apiKey: 'AIzaSyCdtOGarihoROHd5RzK4jBXdr6b8g6xit4'
